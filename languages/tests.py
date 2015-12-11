2d4f61c1c23c966afe4da129c6ddd6f4ef992f6d
Matthew Makai <matthew.makai@gmail.com>
2015-12-11 17:14:34 -0500

    adding one (failed) test

--- languages/tests.py
+++ languages/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory, APITestCase
+from .views import LanguageDetail
 
-# Create your tests here.
+
+emerald = {'name': 'Emerald', 'summary': 'A combination of Python and Ruby.',
+           'homepage_url': 'http://www.fullstackpython.com/'}
+
+
+class ProgrammingLanguagesTest(APITestCase):
+    """
+        Ensures the /programming-languages/ endpoint properly retrieves
+        and creates programming languages.
+    """
+
+    def setUp(self):
+        self.factory = APIRequestFactory()
+
+    def test_create_language(self):
+        request = self.factory.post('/programming-languages/', emerald,
+                                    format='json')
+        response = LanguageDetail.as_view()(request)
+        self.assertEquals(response.status_code, 200)
